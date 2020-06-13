48af23118505f96503fc066f0898bd69d77f63ae
Leo Mancini <mancini@leo.gd>
2020-06-13 16:31:02 -0700

    Debugging

--- resources/helpers/render-list.php
+++ resources/helpers/render-list.php
@@ -521,7 +521,8 @@
 				// if($neighborhood_info_set[$place["foursquare_id"]]["neighborhood_long_name"]) {
 				// 	$places_info[$place["id"]]["neighborhood"] = $neighborhood_info_set[$place["foursquare_id"]]["neighborhood_long_name"];
 				// }
-				$places_info[$place["id"]]["neighborhood"] = 'asd';
+				echo $places_info[$place["id"]];
+				// $places_info[$place["id"]]["neighborhood"] = 'asd';
 			
 				// form category urls
 				$this_categories_urls = generate_category_urls($this_categories_names);
