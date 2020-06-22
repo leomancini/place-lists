9faf01fb395b4cd0fc18ce28a04b3d236528a421
Leo Mancini <mancini@leo.gd>
2020-06-22 09:59:31 -0700

    Debugging

--- list.php
+++ list.php
@@ -1,4 +1,8 @@
 <?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+
 	require("base.php");
 	require("render-list.php");
 	include("../styles/map-style.php");
