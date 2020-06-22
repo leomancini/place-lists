4b2f86d381ae7c7d40faca0de79e978df9970cdf
Leo Mancini <mancini@leo.gd>
2020-06-22 09:58:02 -0700

    Debugging

--- resources/helpers/render-list.php
+++ resources/helpers/render-list.php
@@ -667,12 +667,12 @@
 		
 				// render list name
 				echo "<h1><a href='".$root.$list_name_url_without_neighborhood."'>".$list["name"]."</a></h1>";
-		
-				// render category breadcrumbs
-				render_category_breadcrumbs($url_category_names, $this_categories_urls, $number_of_places);
 				
 				echo "<div id='empty-search-results'>Nothing found...<br><a id='clear-search'>clear search</a></div>";
-		
+
+				// render category breadcrumbs
+				render_category_breadcrumbs($url_category_names, $this_categories_urls, $number_of_places);
+						
 			echo "</div>"; // close #list-header
 		
 			// render sub category index
