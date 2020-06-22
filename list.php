e318e817707a12fe58fd761ec658dbdaefc6a43c
Leo Mancini <mancini@leo.gd>
2020-06-22 10:13:44 -0700

    Debugging

--- list.php
+++ list.php
@@ -1,7 +1,7 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 1);
+	// ini_set('display_startup_errors', 1);
 
 	require("base.php");
 	require("render-list.php");
