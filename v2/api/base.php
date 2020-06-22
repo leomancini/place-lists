49839e868a758e2ef11ba40ed6db3dd71540f330
Leo Mancini <mancini@leo.gd>
2020-06-21 22:49:54 -0700

    Hiding errors

--- v2/api/base.php
+++ v2/api/base.php
@@ -11,9 +11,9 @@
     
     date_default_timezone_set('America/Los_Angeles');
 
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
 
 	function convert($type, $direction, $old_string) {
 		$new_string = $old_string;
